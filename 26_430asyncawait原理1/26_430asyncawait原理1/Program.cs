a420761ecd2254c9f2eac29493db49a79aee3691
whcyyds <wenhaichao0@gmail.com>
2026-05-03 20:46:32 +0800

    异步编程初步了解

---
+++ 26_430asyncawait原理1/26_430asyncawait原理1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _26_430asyncawait原理1
+{
+    internal class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            using(HttpClient httpClient = new HttpClient())
+            {
+               string html = await httpClient.GetStringAsync("https://www.taobao.com");
+                Console.WriteLine(html);
+            }
+            string txt = "小文要努力";
+            string filename = @"e:\temp\a.txt";
+            await File.WriteAllTextAsync(filename, txt);
+            Console.WriteLine("写入成功");
+            string s =await File.ReadAllTextAsync(filename);
+            Console.WriteLine("文件内容："+s);
+        }
+    }
+}
