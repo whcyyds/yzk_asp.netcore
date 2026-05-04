c0580a1af121fe45b287decc82e5720093646864
whcyyds <wenhaichao0@gmail.com>
2026-05-04 20:57:46 +0800

    异步学习

---
+++ 0504_没有async的异步方法/0504_没有async的异步方法/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _0504_没有async的异步方法
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string s = await ReadAsync(1);
+            Console.WriteLine(s);
+        }
+        /*
+         static async Task<string> ReadAsync(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    string s1 = await File.ReadAllTextAsync(@"E:\A NEW LIFE\杨中科\yzk_asp.netcore\resources\1.txt"); return s1;
+                case 2:
+                    string s2 = await File.ReadAllTextAsync(@"E:\A NEW LIFE\杨中科\yzk_asp.netcore\resources\2.txt"); return s2;
+                default: throw new ArgumentException();
+
+            }
+        }
+         */
+
+        static  Task<string> ReadAsync(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return File.ReadAllTextAsync(@"E:\A NEW LIFE\杨中科\yzk_asp.netcore\resources\1.txt");  
+                case 2:
+                    return File.ReadAllTextAsync(@"E:\A NEW LIFE\杨中科\yzk_asp.netcore\resources\2.txt"); 
+                default: throw new ArgumentException();
+
+            }
+        }
+
+
+    }
+}
---
+++ 异步不要用sleep_作业/异步不要用sleep_作业/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace 异步不要用sleep_作业
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
