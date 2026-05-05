91744f485458dca9bf45964aba783237c93f7f93
whcyyds <2398229294@qq.com>
2026-05-05 21:24:00 +0800

    '2'

---
+++ 25_0503异步方法不等于多线程/异步方法不等于多线程/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace 异步方法不等于多线程
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            Console.WriteLine("before:" + Thread.CurrentThread.ManagedThreadId);
+            double r = await CalcAsync(10);
+            Console.WriteLine($"r={r}");
+            Console.WriteLine("after:" + Thread.CurrentThread.ManagedThreadId);
+             
+
+        }
+
+
+        public static async Task<double> CalcAsync(int n)
+        {
+            /*同一个线程
+            Console.WriteLine("CalcAsync:"+Thread.CurrentThread.ManagedThreadId);
+            double result = 0;
+            Random rand = new Random();
+            for(int i = 0; i < n * n; i++)
+            {
+                result += rand.NextDouble();
+            }
+            return result;
+            */
+            //不同线程
+            return await Task.Run(() => {
+                Console.WriteLine("CalcAsync:" + Thread.CurrentThread.ManagedThreadId);
+                double result = 0;
+                Random rand = new Random();
+                for (int i = 0; i < n * n; i++)
+                {
+                    result += rand.NextDouble();
+                }
+                return result;
+            });
+        }
+    }
+}
---
+++ 26_0503异步方法的线程问题/26_503异步方法的线程问题/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace _26_503异步方法的线程问题
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <20; i++)
+            {
+                sb.Append("我要成为高富帅"); 
+            }
+            await File.WriteAllTextAsync("test.txt", sb.ToString());
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
