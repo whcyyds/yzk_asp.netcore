86e970d2ef848a342f2b87a3d3b2a23c98e73a73
whcyyds <2398229294@qq.com>
2026-05-10 23:53:56 +0800

    linq学习

---
+++ 26_0510LINQ常用扩展方法/26_0510LINQ常用扩展方法/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _26_0510LINQ常用扩展方法
+{
+     class Dog
+    {
+        public string NickName;
+
+        public int Age;
+
+        public override string ToString()
+        {
+            return $"xName={NickName},Age={Age}";
+        }
+    }
+}
--- 26_0510LINQ常用扩展方法/26_0510LINQ常用扩展方法/Program.cs
+++ 26_0510LINQ常用扩展方法/26_0510LINQ常用扩展方法/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _26_0510LINQ常用扩展方法
+﻿using System.Collections;
+
+namespace _26_0510LINQ常用扩展方法
 {
     internal class Program
     {
@@ -13,9 +15,64 @@
             list.Add(new Employee { Id = 6, Name = "nancy", Age = 35, Gender = false, Salary = 8000 });
             list.Add(new Employee { Id = 7, Name = "zack", Age = 35, Gender = true, Salary = 8500 });
             list.Add(new Employee { Id = 8, Name = "jack", Age = 33, Gender = true, Salary = 8000 });
-            Employee e1 =  list.Where(r=>r.Name == "a").SingleOrDefault();
-            Console.WriteLine(e1==null);
-            Console.ReadLine();
+
+
+            /*Employee e1 =  list.Where(r=>r.Name == "a").SingleOrDefault();
+             Console.WriteLine(e1==null);
+             Console.ReadLine();*/
+
+            /*IEnumerable<Employee> list1 = list.OrderBy(r => r.Age).ThenByDescending(r=>r.Salary);
+            foreach(Employee e in list1)
+            {
+                Console.WriteLine(e);
+            }*/
+
+            //var items =list.Skip(1).Take(10).ToList();
+            //foreach (Employee e in items)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            //IEnumerable<IGrouping<int,Employee>> listgroup = list.GroupBy(r=>r.Age);
+            //foreach(IGrouping<int,Employee> group in listgroup)
+            //{
+            //    Console.WriteLine(group.Key);
+            //    foreach(Employee item in group)
+            //    {
+            //        Console.WriteLine(item);
+
+            //    }
+            //    Console.WriteLine("------------------------------------------------------");
+
+            //}
+
+            //投影集合
+            //IEnumerable<Dog> select1 = list.Select(x => new Dog{ NickName=x.Name, Age=x.Age });
+            //foreach (var item in select1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var select2 = list.Select(x => new { x.Name, x.Age });
+            //foreach(var item in select2)
+            //{
+            //    Console.WriteLine(item);    
+            //}
+
+            IEnumerable<IGrouping<int, Employee>> listgroup = list.GroupBy(r => r.Age);
+            var i1 = listgroup.Select(r => new
+            {
+                age=r.Key,
+                maxs=r.Max(x => x.Salary)
+            }).OrderBy(r=>r.age);
+            foreach( var i in i1)
+            {
+                Console.WriteLine(i.age);
+                Console.WriteLine(i.maxs);
+                Console.WriteLine("-----------------------------------------");
+            }
+
+
         }
     }
 }
